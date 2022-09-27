44e30b533b261470ef40840abcb3581788c52fc5
jbrillon <43619715+jbrillon@users.noreply.github.com>
2022-09-27 14:36:05 -0400

    progress towards nondimensionalization

--- converter.m
+++ converter.m
@@ -2,18 +2,51 @@ function converter()
 
 A   = importdata('velocity.fld');
 
-B   = A(:,1);
-B   = [B A(:,2)];
-B   = [B A(:,3)];
+% Coordinates
+B   = A(:,1); % x
+B   = [B A(:,2)]; % y
+B   = [B A(:,3)]; % z
 
-C   = A(:,4);
-C   = [C A(:,5)];
-C   = [C A(:,6)];
+% Fluctuating velocity components
+C   = A(:,4); % u
+C   = [C A(:,5)]; % v
+C   = [C A(:,6)]; % w
 
+% Compute the dot product of the velocity fluctation vector at each point
 for i=1:max(size(C))
-    C(i,4) = 0.5*(C(i,1)^2+C(i,2)^2+C(i,3)^2);
+    C(i,4) = (C(i,1)^2+C(i,2)^2+C(i,3)^2);
 end
-M = mean(C(:,4));
+
+turbulent_kinetic_energy = 0.5*mean(C(:,4)); % k
+disp('r.m.s. turbulence intensity:')
+rms_turbulence_intensity = sqrt((2.0/3.0)*turbulent_kinetic_energy) % u' or u_{r.m.s.}
+
+% From experiment
+freestream_velocity_from_experiment = 10.0; % [m/s]
+rms_turbulence_intensity_from_experiment = 0.222; % [m/s] 22.2cm/s
+mesh_size_from_experiment = 0.0508; % [m] 5.08cm
+% mesh_based_reynolds_number = 34000.0;
+% kinematic_viscosity_experiment = (freestream_velocity_from_experiment*mesh_size_from_experiment)/mesh_based_reynolds_number;
+taylor_microscale_from_experiment = 0.00484 % [m]
+integral_lengthscale_from_experiment = 0.0127 % [m]
+kinematic_viscosity_experiment = 27.3*(integral_lengthscale_from_experiment/taylor_microscale_from_experiment)
+
+
+% Reference values
+size_of_computational_box = 11.0*mesh_size_from_experiment; % L=11M
+reference_length = size_of_computational_box/(2.0*pi);
+reference_velocity = sqrt(3.0/2.0)*rms_turbulence_intensity_from_experiment;
+reference_time = reference_length/reference_velocity;
+reference_reynolds_number = (reference_velocity*reference_length)/kinematic_viscosity_experiment;
+
+% Nondimensional spectra output times for the computational model
+nondim_times_experiment = [42.0 98.0 171.0] - 42.0*ones(3); % normalized to t*=42
+dim_times_experiment = (mesh_size_from_experiment/freestream_velocity_from_experiment)*nondim_times_experiment;
+nondim_times_cfd = dim_times_experiment/reference_time
+% TO DO: Write these to the setup_more.dat
+
+
+M = 0.5*mean(C(:,4));
 
 disp('Mach Number')
 V0 = sqrt(2*M)
