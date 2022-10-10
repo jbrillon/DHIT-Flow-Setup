ecfb04336e8dcdaaee3bc4bd23e78ac9a9891ccc
jbrillon <43619715+jbrillon@users.noreply.github.com>
2022-10-10 17:40:27 -0400

    comment in command.sh

--- commands.sh
+++ commands.sh
@@ -5,4 +5,4 @@
 gfortran -malign-double -ffixed-line-length-144 box.for -o main.exe
 # echo -e "box.in" | ./main.exe
 echo -e "box.in\n 1" | ./main.exe # generate the expected coordinates input
-echo -e "box.in\n 0" | ./main.exe
+echo -e "box.in\n 0" | ./main.exe # run code as normal
